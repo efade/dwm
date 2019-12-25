4fd855e7785063c609bbee51735a54a12a19d368
Fakhruddin Ahmad Darwis <fakhri.satu@gmail.com>
2019-12-25 11:05:11 +0700

    Changed the keybinding for scratchpad and its dimension.

--- config.h
+++ config.h
@@ -72,7 +72,7 @@ static const char *roficmd[]    = { "rofi", "-show", "run", NULL};
 
 /* scracthpad commands */
 static const char scratchpadname[] = "scratchpad";
-static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
+static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x28", NULL };
 
 /* volume control for pulse compatible */
 static const char *upvol[]      = { "amixer", "-q", "sset", "Master", "1%+", NULL };
@@ -87,7 +87,7 @@ static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = roficmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_grave,  togglescratch,  {.v = scratchpadcmd } },
+	{ MODKEY,                       XK_u,      togglescratch,  {.v = scratchpadcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_n,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_e,      focusstack,     {.i = -1 } },
