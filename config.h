ebd0733fc954044b51b2a8d21c0056ac368a7eba
Fakhruddin Ahmad Darwis <fakhri.satu@gmail.com>
2019-12-25 11:26:31 +0700

    Added uselessgap

--- config.h
+++ config.h
@@ -3,7 +3,8 @@
 #include <X11/XF86keysym.h> /* required for multimedia keys use */
 
 /* appearance */
-static const unsigned int borderpx  = 5;        /* border pixel of windows */
+static const unsigned int borderpx  = 4;        /* border pixel of windows */
+static const unsigned int gappx     = 1;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
