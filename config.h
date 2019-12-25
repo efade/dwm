8042e1ac31775207b2d330fc5b2ceeb52a78efd8
Fakhruddin Ahmad Darwis <fakhri.satu@gmail.com>
2019-12-25 10:48:27 +0700

    Added scratchpad

--- config.h
+++ config.h
@@ -70,6 +70,10 @@ static const char *dmenucmd[]   = { "dmenu_run", "-m", dmenumon, "-fn", dmenufon
 static const char *termcmd[]    = { "st", NULL };
 static const char *roficmd[]    = { "rofi", "-show", "run", NULL};
 
+/* scracthpad commands */
+static const char scratchpadname[] = "scratchpad";
+static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
+
 /* volume control for pulse compatible */
 static const char *upvol[]      = { "amixer", "-q", "sset", "Master", "1%+", NULL };
 static const char *downvol[]    = { "amixer", "-q", "sset", "Master", "1%-", NULL };
@@ -83,6 +87,7 @@ static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = roficmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_grave,  togglescratch,  {.v = scratchpadcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_n,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_e,      focusstack,     {.i = -1 } },
